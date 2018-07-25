85eb910f3ddf07581e448f5777d6bae5de78e7e4
empyrean987 <knoxm1@gmail.com>
2018-07-24 23:32:51 -0500

    updating

--- cookbooks/chef_wordpress/recipes/default.rb
+++ cookbooks/chef_wordpress/recipes/default.rb
@@ -3,4 +3,59 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-include_recipe "wordpress::apache"
+apt_update 'update'
+
+apt_package 'php7.0-fpm' do
+  action :install
+end
+
+apt_package 'php7.0-mysql' do
+  action :install
+end
+
+apt_package 'php7.0-curl' do
+  action :install
+end
+
+apt_package 'php7.0-gd' do
+  action :install
+end
+
+apt_package 'php7.0-mbstring' do
+  action :install
+end
+
+apt_package 'php7.0-mycrypt' do
+  action :install
+end
+
+apt_package 'php7.0-xml' do
+  action :install
+end
+
+apt_package 'php7.0-xmlrpc' do
+  action :install
+end
+
+apt_package 'apache2' do
+  action :install
+end
+
+directory '/home/ubuntu/wordpress' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/ubuntu/wordpress_latest.tar.gz' do
+  source 'https://wordpress.org/latest.tar.gz'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+execute 'untar_wordpress' do
+  command 'tar xvf /home/ubuntu/wordpress_latest.tar.gz -C /home/ubuntu/wordpress/'
+end
